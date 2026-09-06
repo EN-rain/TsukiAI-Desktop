3c3b3906846fc303c38215ca0afe052c68f0447e
EN-rain <nieves.edriancit2011@gmail.com>
2026-09-06 14:31:22 +0800

    debug: log tone engine wav size

--- TsukiAI.Core/Voice/VoiceToneEngine.cs
+++ TsukiAI.Core/Voice/VoiceToneEngine.cs
@@ -199,6 +199,8 @@ public static class VoiceToneEngine
 
         queryJson = PatchQuery(queryJson, tone, speedFactor);
 
-        return await voicevox.SynthesizeFromQueryAsync(queryJson, styleId, ct, correlationId);
+        var wav = await voicevox.SynthesizeFromQueryAsync(queryJson, styleId, ct, correlationId);
+        DevLog.WriteLine("VoiceToneEngine: wav={0} bytes, style={1}, tone={2}, speedFactor={3}, est={4}", wav.Length, styleId, tone, speedFactor, EstimateDurationSeconds(queryJson).ToString("0.00"));
+        return wav;
     }
 }
